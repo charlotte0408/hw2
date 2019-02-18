cc956e10f7cb10eec1ba9b78d0c56064b517c4dd
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-02-18 09:54:36 -0800

    Set up the schemaB

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -8,7 +8,9 @@ grade_calculator::grade_calculator(QWidget *parent) :
     ui->setupUi(this);
     score = 0;
     connect(ui->radioButton,SIGNAL(clicked()),this,SLOT(schemaA()));
+    connect(ui->radioButton_2,SIGNAL(clicked()),this,SLOT(schemaB()));
     connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(display()));
+
 }
 
 grade_calculator::~grade_calculator()
@@ -26,10 +28,29 @@ void grade_calculator::schemaA()
     int mid1 = ui->spinBox_8->value();
     int mid2 = ui->spinBox_9->value();
     int final = ui->spinBox_10->value();
-    score = (hw1+hw2+hw3+hw4+hw5)*0.25+0.2*mid1+0.2*mid2+0.35*final;
+    score = (hw1+hw2+hw3+hw4+hw5)/5*0.25+0.2*mid1+0.2*mid2+0.35*final;
+}
+
+void grade_calculator::schemaB()
+{
+    int hw1 = ui->spinBox->value();
+    int hw2 = ui->spinBox_2->value();
+    int hw3 = ui->spinBox_3->value();
+    int hw4 = ui->spinBox_4->value();
+    int hw5 = ui->spinBox_5->value();
+    int mid1 = ui->spinBox_8->value();
+    int mid2 = ui->spinBox_9->value();
+    int mid = 0;
+    if (mid1 > mid2)
+        mid = mid1;
+    else
+        mid = mid2;
+    int final = ui->spinBox_10->value();
+    score = (hw1+hw2+hw3+hw4+hw5)/5*0.25+0.3*mid+0.44*final;
 }
 
 void grade_calculator::display()
 {
     ui->label->setText(QString::number(score));
 }
+
--- grade_calculator.h
+++ grade_calculator.h
@@ -17,6 +17,7 @@ public:
 
 public slots:
     void schemaA();
+    void schemaB();
     void display();
 
 private:
